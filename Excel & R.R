577c43a63311c030eb746da11d8377504b0a77c3
Jordi Mas <jordimas@Mello-Yello-2.local>
2022-12-08 10:04:38 +0100

    Slides ER

---
+++ Excel & R.R
@@ -0,0 +1,7 @@
+install.packages("gapminder")
+library(pacman)
+p_load(tidyverse, gapminder)
+write_csv(gapminder, "gapminder.csv")
+write_csv2(gapminder, "gapminder2.csv")
+write_tsv(gapminder, "gapminder3.tsv")
+write_delim(gapminder, "gapminder4.txt", delim = "/")
