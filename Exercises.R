9d8b96395a618a387107cd245eb5cfab093c8f3d
Jordi Mas <jordimas@Mello-Yello-2.local>
2023-01-19 23:53:41 +0100

    DF

--- Exercises.R
+++ Exercises.R
@@ -1,21 +1,16 @@
-
-
-## FUNCTIONS
-
-aa <- function(hello = 4) {hello + 1}
-aa()
-
-bb <- function(hello, bye = 0) {hello + bye}
-bb(hello = 3)
-
-cc <- function(hello, bye = 0) {hello + bye}
-cc(3, 3)
-
-dd <- function(x, y) {x * y}
-dd(1, 3)
-
-ee <- function(x, y = 1) {x * y}
-ee(1)
+## Nominals
 
 
 
+## Stringr
+
+
+
+## Ordinal
+
+
+
+# Forcats
+
+
+## Interval
--- Observations_Variables.R
+++ Observations_Variables.R
@@ -15,9 +15,9 @@
 #     - Normally we put the variables in the columns of a dataset.
 
 # Concepts
-"Table" == "Dataframe"
-"Table" == "Dataset"
-"Dataframe" == "Dataset"
+"Table" > "Dataframe"
+"Table" > "Dataset"
+"Dataframe" < "Dataset"
 
 "Variable" == "Column" #not always
 "Observation" == "Row" #not always
@@ -32,20 +32,11 @@
 #     - Unit of observation
 
 
-# PART 1.1. DATAFRAMES
-
-#Gapminder
-library(gapminder)
-gapminder
-
-# https://correlatesofwar.org/data-sets/cow-war/
-read_csv("data/Intra-StateWarData_v4.1.csv")
-
-# https://militarycoups.org/
-read_csv("data/cam_list_3.0.csv")
+# 1.1. OBSERVATIONS
+# Check the slides
 
 
-# PART 1.2. ECOLOGICAL INFERENCE PROBLEM
+# 1.2. ECOLOGICAL INFERENCE PROBLEM
 
 # More info: https://en.wikipedia.org/wiki/Simpson%27s_paradox
 
@@ -53,13 +44,12 @@ read_csv("data/cam_list_3.0.csv")
 library(dplyr)
 library(tidyr)
 
-# The Simpson's paradox
+# 1.2.1. UC BERKELEY
 # Tibble with admissions to different undergraduate programmes
 uni <- tibble(faculty = rep(c("Psysics", "Politics", "Journalism"), times = 1, each = 2),
               sex = rep(c("Men", "Women"), times = 3),
               applicants = c(825,108,325,593,181,392),
               admitted = c(520, 76, 119, 203, 52, 115))
-
 uni
 
 
@@ -76,12 +66,13 @@ uni |>
   arrange(desc(perc_admissions))
 
 
-## DURKHEIM SUICIDE
+## 1.2.2. DURKHEIM SUICIDE
 suicide <- tibble(region = c("Isère"),
                   municipality = rep(c("Grenoble", "Le Bourg-d'Oisans", "Saint-Jean-de-Maurienne"), times = 1, each = 2),
                   religion = rep(c("Protestant", "Catholic"), times = 3),
                   population = c(8250,1080,325,593,181,392),
                   suicide = c(520, 72, 12, 20, 5, 11))
+suicide
 
 #What aggregate data (regional level) is telling us?
 suicide |> 
@@ -102,30 +93,187 @@ suicide |>
 #####################################################################
 
 # - Variables: Characteristics of what we study.
-#     - Categoric nominal: No order
-#     - Categoric ordinal: Order, no distance
-#     - Numeric interval: Distance, zero no meaning
-#     - Numeric ratio: Order, distance, zero
+#     - Categorical nominal: No order
+#     - Categorical ordinal: Order, distance not known
+#     - Numeric interval: Order, distance is known, zero no meaning
+#     - Numeric ratio: Order, distance is known, zero has meaning
 
 
-# 2.1. NOMINAL
+## 2.1. NOMINAL
 
-# Categoric nominal: No order
+#     - Categorical nominal: No order
+
+### 2.1.1. DATAFRAME
+strings <- tribble(~iso3c, ~country, ~currency, ~continent, ~region,
+                   "CMR", "Cameroon", "CFA Franc BEAC", "Africa", "Sub-Saharan Africa",
+                   "COL", "Colombia", "Colombian Peso", "Americas", "Latin America & Caribbean",
+                   "CUB", "Cuba", "Cuban Peso", "Americas", "Latin America & Caribbean",
+                   "FRA", "France", "Euro", "Europe", "Europe & Central Asia",
+                   "LSO", "Lesotho", "Loti", "Africa", "Sub-Saharan Africa",
+                   "QAT", "Qatar", "Qatari Rial", "Asia", "Middle East & North Africa",
+                   "TWN", "Taiwan", "New Taiwan Dollar", "Asia", "East Asia & Pacific",
+                   "TTO", "Trinidad & Tobago", "Trinidad & Tobago Dollar", "Americas", "Latin America & Caribbean")
+strings
+
+
+### 2.1.2. STORAGE
+#    - Character
+#    - Factor
+
+
+### 2.1.3. OPERATIONS
+#    - Equality: ==
+#    - Equality: %in%
+#    - Not equality: !=
+
+strings$country == "France"
+factor(strings$country) == "France"
+strings$continent %in% c("Africa", "Americas", "Asia")
+strings$currency != "Euro"
+!strings$currency %in% c("Euro", "Loti", "Cuban Peso")
+
+
+#Ergo
+strings[strings$continent %in% c("Africa", "Europe"),]
+sum(strings$region == "Latin America & Caribbean")
+
+
+### 2.1.4. EXERCISE
+
+# Show me the observations from Argentina
+# How many observations from Colombia?
+glimpse(ches_la)
+
+## 2.1.5. BINARY VARIABLES
+
+#Also called dummy or dichotomous.
+
+ches_la$president
+
+#red:
+#elected:
+#democracy:
+#alliance: 
+#successful coup d'état
+#reg_aut of an ethnic group
+
+
+
+### 2.1.6. STRINGR PACKAGE
+
+library(stringr)
+str_remove(strings$region, "Latin ")
+str_replace(strings$country, "&", "and")
+str_to_upper(strings$currency)
+str_sub(strings$iso3c, end = 2)
 
 
 
 
+## 2.2. ORDINAL
+
+#     - Categorical ordinal: Order, distance not known
+
+### 2.2.1. DATAFRAME
+ords <- tibble(donor = c("US-MCC", "Canada-GAC", "Germany-BMZ-GIZ", "Korea-KOICA", "Australia-DFAT", "Spain-AECID", 
+                         "Saudi Arabia-KSRelief", "Norway-MFA", "China-MOFCOM", "Turkey-TIKA"),
+               ati = factor(c("Very Good", "Good", "Good", "Good", "Good", "Fair",
+                              "Poor", "Poor", "Very Poor", "Very Poor"),
+                            ordered = TRUE,
+                            levels = c("Very Poor", "Poor", "Fair", "Good", "Very Good")),
+               regime_type = factor(c("Flawed Democracy", "Full Democracy", "Full Democracy", "Full Democracy", "Full Democracy", 
+                                      "Flawed Democracy", "Authoritarian", "Full Democracy", "Authoritarian", "Hybrid Regime"), 
+                                    ordered = TRUE,
+                                    levels = c("Authoritarian", "Hybrid Regime", "Flawed Democracy", "Full Democracy")))
+ords
+
+### 2.2.2. STORAGE
+#    - Ordered factor
+
+ords$ati <- factor(ords$ati,
+                   ordered = TRUE,
+                   levels = c("Very Poor", "Poor", "Fair", "Good", "Very Good"))
+
+ords$ati
+class(ords$ati)
+typeof(ords$ati)
+unclass(ords$ati)
 
 
-# 2.2. ORDINAL
+### 2.2.3. OPERATIONS
+#    - Equality: ==
+#    - Equality: %in%
+#    - Not equality: !=
+#    - More than: >
+#    - More or equal than: >=
+#    - Less than: <
+#    - Less or equal than: <=
+
+
+ords$regime_type >= "Hybrid Regime"
+
+ords[ords$regime_type != "Full Democracy",]
+ords[ords$ati >= "Good",]
+ords[ords$ati < "Fair",]
+
+
+### 2.2.4. FORCATS PACKAGE
+library(forcats)
+fct_relevel(ords$regime_type, "Hybrid Regime", "Full Democracy", "Flawed Democracy", "Authoritarian")
+fct_rev(ords$ati)
+fct_other(ords$ati, keep = c("Good", "Very Good"))
+plot(fct_infreq(ords$ati))
+plot(fct_rev(fct_infreq(ords$ati)))
 
 
 
-# 2.3. INTERVAL
+## 2.3. INTERVAL
+
+#     - Numeric interval: Order, distance is known, zero no meaning
+
+
+### 2.3.1. DATAFRAME
+polity <- tibble(country = c("United States", "Bolivia", "Australia", "Azerbaijan",
+                             "USSR", "Timor Leste", "Eritrea", "Qatar", "Gambia"),
+                 year = c(1776, 1825, 1901, 1991, 1922, 2002, 1993, 1971, 1965),
+                 polity2 = c(0, -3, 10, -3, -7, 6, -6, -10, 8))
+polity
+
+
+### 2.3.2. STORAGE
+#    - Numeric
+#    - Integer
+#    - Date
+
+
+### 2.3.3. OPERATIONS
+#    - Equality: ==
+#    - Equality: %in%
+#    - Not equality: !=
+#    - More than: >
+#    - More or equal than: >=
+#    - Less than: <
+#    - Less or equal than: <=
+#    - Sometimes, sums and differences: +, -
 
 
 
-# 2.4. RATIO
+# Difference between the foundation of the US and the foundation of the USSR
+polity$year[polity$country == "United States"] - polity$year[polity$country == "USSR"]
+
+# Which political regimes had more democracy than the US in the year of their foundation?
+polity$country[polity$polity2[polity$country == "United States"] > polity$polity2]
+
+
+
+## 2.4. RATIO
+
+#     - Numeric ratio: Order, distance is known, zero has meaning
+
+
+### 2.4.1. DATAFRAME
+### 2.4.2. STORAGE
+### 2.4.3. OPERATIONS
 
 
 
