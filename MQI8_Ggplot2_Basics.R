89be96ad7c9e7a3ba31e0f6b2e061f157efbb883
Jordi Mas <jordimas@Mello-Yello-2.local>
2023-02-14 21:20:41 +0100

    Ggplot

--- MQI8_Ggplot2_Basics.R
+++ MQI8_Ggplot2_Basics.R
@@ -49,15 +49,20 @@ cens_gc #UO: individu
 #####################################################################
 
 
-## 1.1. 
+## 1.1. LAYERS
+
+
+accidents |> 
+  ggplot(aes(x = nom_mes, y = edat)) +
+  geom_col()
 
 
 ## 1.2. 
 
-facet
-coord_flip
-theme
-labs
+#facet
+#coord_flip
+#theme
+#labs
 
 
 #####################################################################
@@ -90,67 +95,8 @@ municipi |>
   ggplot(aes(x = comarca)) +
   geom_bar()
 
-accidents |> 
-  ggplot(aes(x = nom_districte)) +
-  geom_bar()
 
 
-
-
-## 2.3. NUMERICAL AND CATEGORICAL
-
-### 2.3.1. GEOM_COL
-
-municipi |> 
-  ggplot(aes(x = comarca, y = poblacio)) +
-  geom_col()
-
-contractes_menors |> 
-  ggplot(aes(x = tipus_ens, y = import_adjudicat)) +
-  geom_col()
-
-## 2.3.2. GEOM_BOXPLOT
-elecc19 |>
-  filter(nombre_de_comunidad == "Andalucía") |> 
-  mutate(cs_per = cs / poblacion * 100) |> 
-  ggplot(aes(x = nombre_de_provincia, y = cs_per)) +
-  geom_boxplot()
-
-## 2.3.3. GEOM VIOLIN (EXTRA - no exam)
-elecc19 |>
-  filter(nombre_de_comunidad == "Andalucía") |> 
-  mutate(cs_per = cs / poblacion * 100) |> 
-  ggplot(aes(x = nombre_de_provincia, y = cs_per)) +
-  geom_violin()
-
-## 2.3.4. GEOM_POINT (EXTRA - no exam)
-lloguer_any |> 
-  group_by(nom_districte) |>
-  summarize(preu_m2 = round(mean(preu_m2, na.rm = T), 1)) |> 
-  ggplot(aes(x = fct_reorder(nom_districte, preu_m2), y = preu_m2)) +
-  geom_point(size = 10, col = "darkblue") +
-  geom_text(aes(label = preu_m2), col = "white") +
-  coord_flip() +
-  theme_minimal() +
-  labs(x = NULL, y = NULL)
-
-
-## 2.4. NUMERICAL ACROSS TIME
-
-### 2.4.1. GEOM LINE
-lloguer_any |> 
-  group_by(any) |> 
-  summarize(preu = mean(preu, na.rm = T)) |> 
-  ggplot(aes(x = any, y = preu)) +
-  geom_line() +
-  theme_minimal()
-
-lloguer_any |> 
-  group_by(any, nom_districte) |> 
-  summarize(preu = mean(preu, na.rm = T)) |> 
-  ggplot(aes(x = any, y = preu, col = nom_districte)) +
-  geom_line() +
-  theme_minimal()
 
 
 
